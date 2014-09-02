e1a17aa361e3b6325ab48ff037692c72bc3c6c93
Raphael Nagel <compiaffe@users.noreply.github.com>
2014-09-02 13:54:50 +0100

    compiles and reacts to controller commands, cannot be run at 500Hz but seems to work at 50Hz)

--- copy_bbb_brlnet.sh
+++ copy_bbb_brlnet.sh
@@ -2,7 +2,7 @@
 echo "I'll copy all ADIS_Interface files over to the BBB"
 
 rsync -avzh ./src/*.cpp 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/test_robot/src/
-rsync -avzh ./include/test_robot/*.hpp 	BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/test_robot/include/test_robot/
+rsync -avzh ./include/test_robot/*.hpp 			BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/test_robot/include/test_robot/
 rsync -avzh CMakeLists.txt 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/test_robot/
 rsync -avzh *.xml 								BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/test_robot/
 rsync -avzh *.launch	 						BBB_BRLNET:/home/ubuntu/uwesub_catkin_workspace/src/test_robot/
